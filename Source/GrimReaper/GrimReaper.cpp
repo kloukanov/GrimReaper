7732a486731b4ab3bf4793ca1dad50f0b73f52f8
Krassy Loukanov <kloukanov@outlook.com>
2025-11-02 19:14:54 -0500

    init

---
+++ Source/GrimReaper/GrimReaper.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GrimReaper.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GrimReaper, "GrimReaper" );
